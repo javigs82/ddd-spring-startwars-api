2e913b1246f756402e0460f5ecf29fe38f448eba
javigs82 <javigs82@gmail.com>
2019-03-13 23:50:05 +0100

    refactor to be a typed common result class

--- src/main/java/me/javigs82/startwars/domain/model/ModelResult.java
+++ src/main/java/me/javigs82/startwars/domain/model/ModelResult.java
@@ -1,4 +1,43 @@
 package me.javigs82.startwars.domain.model;
 
-public class ModelResult {
+import java.util.List;
+
+public class ModelResult<T> {
+    private int count;
+    private String next;
+    private String previous;
+    private List<T> results;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public List<T> getResults() {
+        return results;
+    }
+
+    public void setResults(List<T> results) {
+        this.results = results;
+    }
+
 }
